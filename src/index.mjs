d8acfb169a0d6977e2d1eaab48cd7953d660bef7
strlghf <strlghf@gmail.com>
2026-03-04 20:09:35 -0300

    adding functionality into api/users routh

--- src/index.mjs
+++ src/index.mjs
@@ -5,32 +5,17 @@ const app = express();
 const PORT = process.env.PORT || 5173;
 
 const users = [
-  {
-    id: 1,
-    name: "James",
-    age: 23,
-  }, {
-    id: 2,
-    name: "Agus",
-    age: 20
-  }, {
-    id: 3,
-    name: "Valen",
-    age: 33
-  },
-  {
-    id: 7,
-    name: "Lucas",
-    age: 22
-  }, {
-    id: 77,
-    name: "Alis",
-    age: 31
-  }
+  { id: 1, name: "James", age: "33" }, 
+  { id: 2, name: "Agus", age: 20 }, 
+  { id: 3, name: "Valen", age: 33 },
+  { id: 7, name: "Lucas", age: 22 }, 
+  { id: 77, name: "Alis", age: 31, husband: "Lucas", verse: "Mateo 6:33", favoriteFood: "Chocolate", cat: "Pelu" },
+  { id: 53, name: "André", age: 2 },
+  { id: 43, name: "Salomon", age: 43 }
 ]
 
 app.get("/", (req, res) => {
-  return res.status(200).send("<h1>Hello!</h1>")
+  return res.status(200).send("Hello")
 })
 
 app.get("/alis", (req, res) => {
@@ -38,8 +23,14 @@ app.get("/alis", (req, res) => {
 })
 
 app.get("/api/users", (req, res) => {
-  
-  return res.send(JSON.stringify(users))
+  const { query: { filter, value } } = req
+
+  // when filter and value are undefined
+  if (filter && value) {
+    return res.send(users.filter(user => user[filter].includes(value)))
+  }
+
+  return res.send({ users })
 })
 
 app.get("/api/products", (req, res) => {
