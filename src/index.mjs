ccd79bb196f3991d37cb05cfa5804ea45c360a41
strlghf <strlghf@gmail.com>
2026-03-04 14:44:54 -0300

    practice endpoints

--- src/index.mjs
+++ src/index.mjs
@@ -4,12 +4,49 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const users = [
+  {
+    id: 1,
+    name: "James",
+    age: 23,
+  }, {
+    id: 2,
+    name: "Agus",
+    age: 20
+  }, {
+    id: 3,
+    name: "Valen",
+    age: 33
+  }
+]
+
 app.get("/", (req, res) => {
-  return res.end("Hello")
+  return res.status(200).send("<h1>Hello!</h1>")
 })
 
 app.get("/alis", (req, res) => {
   return res.end("I loveee i lovee aa")
+})
+
+app.get("/api/users", (req, res) => {
+  return res.send(users)
+})
+
+app.get("/api/products", (req, res) => {
+  res.send([{ id: 123, name: "chicken breast", price: 12.99 }])
+})
+
+app.get("/api/users/:id", (req, res) => {
+  const { id } = req.params
+  const parsedId = parseInt(id)
+
+  if (isNaN(parsedId)) return res.status(400).send({ msg: "Bad Request. Invalid ID" })
+
+  const findUser = users.find(user => user.id === parsedId)
+
+  if (!findUser) return res.sendStatus(404)
+
+  res.send(findUser)
 })
 
 app.listen(PORT, () => {
