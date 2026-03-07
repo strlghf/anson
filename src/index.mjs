6600d813bcba0ad34797b1d12a18a6eb772ff590
strlghf <strlghf@gmail.com>
2026-03-06 23:10:46 -0300

    patch method

--- src/index.mjs
+++ src/index.mjs
@@ -74,6 +74,21 @@ app.put("/api/users/:id", (req, res) => {
   return res.sendStatus(200)
 })
 
+app.patch("/api/users/:id", (req, res) => {
+  const { body, params: { id } } = req
+
+  const parsedId = +id
+  if (isNaN(parsedId)) return response.sendStatus(400)
+
+  const findUserIndex = users.findIndex(user => user.id === parsedId)
+
+  if (findUserIndex === -1) return res.sendStatus(404)
+
+  users[findUserIndex] = { ...users[findUserIndex], ...body }
+
+  return res.sendStatus(200)
+})
+
 app.listen(PORT, () => {
   console.log(`Running on Port ${PORT}`)
 })
