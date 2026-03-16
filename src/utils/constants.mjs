23fe9467c35c8644a0d3b44d6a34fb1d138c58eb
strlghf <strlghf@gmail.com>
2026-03-16 13:30:40 -0300

    my beloved new password

--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -3,7 +3,7 @@ export const users = [
   { id: 2, name: "Agus", age: 20 }, 
   { id: 3, name: "Valen", age: 33 },
   { id: 7, name: "Lucas", age: 22, password: "lanix123" }, 
-  { id: 77, name: "Alis", age: 31, husband: "Lucas", verse: "Mateo 6:33", favoriteFood: "Chocolate", cat: "Pelu" },
+  { id: 77, name: "Alis", age: 31, password: "mateo77", husband: "Lucas", verse: "Mateo 6:33", favoriteFood: "Chocolate", cat: "Pelu" },
   { id: 53, name: "André", age: 2 },
   { id: 43, name: "Salomon", age: 43 }
 ]
