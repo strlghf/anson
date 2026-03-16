a378514d42d978f0891ed2a0902cc2f3c20d7672
strlghf <strlghf@gmail.com>
2026-03-16 11:06:14 -0300

    finish cookies

--- src/index.mjs
+++ src/index.mjs
@@ -2,29 +2,31 @@ import express from "express";
 import { loggingMiddleware } from "./utils/middlewares.mjs";
 import routes from "./routes/index.mjs"
 import cookieParser from "cookie-parser";
+import session from "express-session";
 
 const app = express();
 
 app.use(express.json())
-app.use(cookieParser())
-app.use(routes)
-app.use(loggingMiddleware)
+app.use(cookieParser("helloworld"));
+app.use(session());
+app.use(routes);
+app.use(loggingMiddleware);
 
 const PORT = process.env.PORT || 5173;
 
 app.get("/", (req, res) => {
-  res.cookie("hello", "world", { maxAge: 60000, signed: true })
-  res.status(200).send("Hello")
+  res.cookie("hello", "world", { maxAge: 60000 })
+  res.status(200).send("Hello");
 })
 
 app.get("/alis", (req, res) => {
-  res.status(200).send("You my love, you are the most beautiful and georgeous woman on the entire planet, your eyes, your smile, your kind heart. I love you with my whole heart, ur existence makes my spirit shine, i will wait for you till the end, because i refuse to marry and have childs with another woman. Forgive me for my weakness. I will forever have you on my heart. I will not surrender with you, i will not give up on you. May God judge my words. God bless our beautiful love and fill us with His love.")
+  res.status(200).send("You my love, you are the most beautiful and georgeous woman on the entire planet, your eyes, your smile, your kind heart. I love you with my whole heart, ur existence makes my spirit shine, i will wait for you till the end, because i refuse to marry and have childs with another woman. Forgive me for my weakness. I will forever have you on my heart. I will not surrender with you, i will not give up on you. May God judge my words. God bless our beautiful love and fill us with His love.");
 })
 
 app.use((req, res) => {
-  res.status(404).send("Resource has been deleted or not found")
+  res.status(404).send("Resource has been deleted or not found");
 })
 
 app.listen(PORT, () => {
-  console.log(`Running on Port ${PORT}`)
+  console.log(`Running on Port ${PORT}`);
 })
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -5,11 +5,12 @@ const router = Router();
 router.get("/api/products", (req, res) => {
   console.log(req.headers.cookie)
   console.log(req.cookies)
+  console.log(req.signedCookies.hello)
   if (req.cookies.hello && req.cookies.hello === "world") {
     return res.send([{ id: 123, name: "chicken breast", price: 12.99 }])
   }
 
-  return res.status(403).send("Sorry. U need to have the correct cookie")
+  return res.status(401).send("Sorry. U need to have the correct cookie")
 })
 
 export default router
