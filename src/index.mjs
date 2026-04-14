91ec9c2afa6d445b21e268da5d87caf2248a2012
strlghf <strlghf@gmail.com>
2026-04-14 16:13:07 -0400

    blank and users endpoints

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,18 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.status(200).send({ msg: "Welcome to the real world" });
+})
+
+app.get("/api/users", (req, res) => {
+  res.send([
+    { id: 1, username: "pheralb", displayName: "Pheralb" },
+    { id: 2, username: "midudev", displayName: "MiduDev" },
+    { id: 3, username: "anson", displayName: "Anson" }
+  ])
+})
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 });
