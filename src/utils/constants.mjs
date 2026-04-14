0ebdd1646656be26763b3d7c3bfa62cb3b0ce547
strlghf <strlghf@gmail.com>
2026-04-14 15:56:45 -0400

    -

--- src/utils/constants.mjs
+++
@@ -1,9 +0,0 @@
-export const users = [
-  { id: 1, name: "James", password: "james123", age: "33" }, 
-  { id: 2, name: "Agus", password: "agus123", age: 20 }, 
-  { id: 3, name: "Valen", password: "valen123", age: 33 },
-  { id: 7, name: "Lucas", age: 22, password: "lanix123" }, 
-  { id: 77, name: "Alis", age: 31, password: "mateo77", husband: "Lucas", verse: "Mateo 6:33", favoriteFood: "Chocolate", cat: "Pelu" },
-  { id: 53, name: "André", password: "andre123", age: 2 },
-  { id: 43, name: "Salomon", password: "salmon123", age: 43 }
-]
