24be775f16e9303745ec19206b9c5ea48f473058
strlghf <strlghf@gmail.com>
2026-04-14 16:21:38 -0400

    .

--- src/index.mjs
+++ src/index.mjs
@@ -4,24 +4,32 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const users = [
+  { id: 1, username: "pheralb", displayName: "Pheralb" },
+  { id: 2, username: "midudev", displayName: "MiduDev" },
+  { id: 3, username: "anson", displayName: "Anson" }
+]
+
+const products = [
+  { id: "123", name: "chicken breast", price: "12.99" },
+  { id: "132", name: "react course", price: "72.99" },
+  { id: "231", name: "red mantle", price: "23.99" }
+]
+
 app.get("/", (req, res) => {
   res.status(200).send({ msg: "Welcome to the real world" });
 })
 
 app.get("/api/users", (req, res) => {
-  res.send([
-    { id: 1, username: "pheralb", displayName: "Pheralb" },
-    { id: 2, username: "midudev", displayName: "MiduDev" },
-    { id: 3, username: "anson", displayName: "Anson" }
-  ])
+  res.send()
 })
 
 app.get("/api/products", (req, res) => {
-  res.send([
-    { id: "123", name: "chicken breast", price: "12.99" },
-    { id: "132", name: "react course", price: "72.99" },
-    { id: "231", name: "red mantle", price: "23.99" }
-  ])
+  res.send()
+})
+
+app.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
 })
 
 app.listen(PORT, () => {
