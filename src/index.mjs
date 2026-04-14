a3fb3d54e047ffa6500879a35620997c8113e9e5
strlghf <strlghf@gmail.com>
2026-04-14 16:17:11 -0400

    products

--- src/index.mjs
+++ src/index.mjs
@@ -16,6 +16,14 @@ app.get("/api/users", (req, res) => {
   ])
 })
 
+app.get("/api/products", (req, res) => {
+  res.send([
+    { id: "123", name: "chicken breast", price: "12.99" },
+    { id: "132", name: "react course", price: "72.99" },
+    { id: "231", name: "red mantle", price: "23.99" }
+  ])
+})
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 });
