5ee042cec3d37e7d0a0157f6141348fa9b80ab50
strlghf <strlghf@gmail.com>
2026-04-16 12:55:31 -0400

    patch

--- src/index.mjs
+++ src/index.mjs
@@ -64,14 +64,24 @@ app.put("/api/users/:id", (req, res) => {
   const parsedId = +id;
   if (isNaN(parsedId)) return res.sendStatus(400);
 
-  const findUserIndex = users.findIndex(
-    user => user.id === parsedId
-  )
+  const findUserIndex = users.findIndex(user => user.id === parsedId)
 
   if (findUserIndex === -1) return res.sendStatus(404);
 
   users[findUserIndex] = { id: parsedId, ...body };
 
+  return res.sendStatus(200);
+})
+
+app.patch("/api/users/:id", (req, res) => {
+  const { body, params: { id } } = req;
+
+  const parsedId = +id;
+  if (isNaN(parsedId)) return res.sendStatus(400);
+
+  const findUserIndex = users.findIndex(user => user.id === parsedId)
+  if (findUserIndex === -1) return res.sendStatus(404);
+  users[findUserIndex] = { ...users[findUserIndex], ...body };
   return res.sendStatus(200);
 })
 
