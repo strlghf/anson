2fc1d038bdd508586fe91c3514b0e263e1aab11f
strlghf <strlghf@gmail.com>
2026-04-22 17:04:35 -0400

    types check

--- src/db/users.repository.ts
+++ src/db/users.repository.ts
@@ -1,5 +1,5 @@
 import { pool } from "./database.js";
-import type { ResultSetHeader } from "mysql2";
+import type { ResultSetHeader, RowDataPacket } from "mysql2";
 
 export interface User {
   id: number;
@@ -8,34 +8,36 @@ export interface User {
   password: string;
 }
 
+type UserRow = User & RowDataPacket;
+
 export async function getUserById(id: number): Promise<User | null> {
-  const [result] = await pool.query<User[]>(
+  const [rows] = await pool.query<UserRow[]>(
     `SELECT id, username, password
      FROM users
      WHERE id = (?);`, [id]
   );
 
-  return result[0] ?? null;
+  return rows[0] ?? null;
 }
 
 export async function getUserByUsername(username: string): Promise<User | null> {
-  const [result] = await pool.query<User[]>(`
+  const [rows] = await pool.query<UserRow[]>(`
     SELECT id, username, password, displayName
     FROM users
     WHERE username = (?);`, [username]
   );
 
-  return result[0] ?? null;
+  return rows[0] ?? null;
 }
 
 export async function createUser ({ username, displayName, password }: User) {
-  const [result] = await pool.query<ResultSetHeader>(`
+  const [rows] = await pool.query<ResultSetHeader>(`
     INSERT INTO users (username, displayName, password)
     VALUES (?, ?, ?);
   `, [username, displayName, password])
 
-  const user = await getUserById(result.insertId);
+  const user = await getUserById(rows.insertId);
   if (!user) throw new Error("User creation failed");
-  
+
   return user
 }
