cc353b5372453bfb9fa36545e1fc57a5cc6ca323
strlghf <strlghf@gmail.com>
2026-04-22 17:00:06 -0400

    repository layer

--- src/strategies/local-strategy.ts
+++ src/strategies/local-strategy.ts
@@ -1,15 +1,14 @@
 import passport from "passport";
 import { Strategy } from "passport-local";
-import {  }
-import { users } from "../utils/constants.js";
+import { getUserById, getUserByUsername } from "../db/users.repository.js";
 
 passport.serializeUser((user: any, done) => {
   done(null, user.id);
 })
 
-passport.deserializeUser(async (id, done) => {
+passport.deserializeUser(async (id: number, done) => {
   try {
-    const findUser = users.find(user => user.id === id);
+    const findUser = await getUserById(id);
     if (!findUser) return done(null, false);
     done(null, findUser);
   } catch (err) {
@@ -20,13 +19,12 @@ passport.deserializeUser(async (id, done) => {
 export default passport.use(
   new Strategy(async (username, password, done) => {
     try {
-      const users = await getUsers();
-      const findUser = users.find(user => user.username === username);
-      if (!findUser) throw new Error("User not found");
-      if (findUser.password !== password) throw new Error("Failed authentication")
-      done(null, findUser);
+      const user = await getUserByUsername(username);
+      if (!user) return done(null, false);
+      if (user.password !== password) return done(null, false);
+      return done(null, user);
     } catch (err) {
-      done(err);
+      return done(err);
     }
   })
 )
